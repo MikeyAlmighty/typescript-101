5430baec55a233034db67cf192448da4889bc90c
Mikey <mike.leibbrandt@gmail.com>
2020-06-25 19:02:28 +0200

    Hooks

--- src/Playground.tsx
+++ src/Playground.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Interface a way to map object properties with types
 interface HeaderProps {
@@ -17,14 +17,25 @@ interface Mammal {
 	fn?: (genus: string) => string; // Optional function that takes in a string param and returns a string
 }
 
+interface TextNode {
+	text: string;
+}
 
 export default function Header({buttonText, age}: HeaderProps){
+	// const [count, setCount] = useState(5) // By Default Type Inferred
 
-	return(
+	const [count, setCount] = useState<number | null>(5) 
+	// setCount(false) Throws Type Error
+
+	const [text, setText] = useState<TextNode>({text: 'DeadPool'})
+	// setText(3) Throws Type Error
+
+	return (
 		<div>
 			<h1>{age}</h1>
 			<button>{buttonText}</button>
 		</div>
 	)
 }
+
 
